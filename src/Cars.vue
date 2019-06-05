0c2714580e8618a87acef801612aab5e4942bbd2
Andrey999 <gromovandrey1985@gmail.com>
2019-06-05 14:57:19 +0300

    add styles and modal

--- src/Cars.vue
+++ src/Cars.vue
@@ -9,15 +9,22 @@
 
             <div class="form">
                 <label for="search">Find your cars</label>
-                <input type="text" class="form__input" id="search" placeholder="name, model ..." />
+                <input
+                        type="text"
+                        class="form__input"
+                        id="search"
+                        placeholder="name, model ..."
+                        v-model="search"/>
 
                 <hr>
 
                 <ul class="list">
                     <li
-                    v-for="(car, index) in cars"
+                    v-for="(car, index) in filteredCars"
                     class="list__item"
-                    @click="selectCar(index)">{{ car.name }} - {{ car.model }}</li>
+                    @click="selectCar(index)"
+                    :class="[ selectedCarIndex === index ? 'activeCarElem' : '' ]">{{ car.name }} - {{ car.model }}
+                    </li>
                 </ul>
             </div>
 
@@ -37,10 +44,10 @@
                         <li>Owner - <strong>{{ car.owner }}</strong></li>
                     </ul>
 
-                    <a class="details__phone" :tel="car.phone">{{ car.phone }}</a>
+                    <a class="details__phone" :class="[ isActiveTel ? '' : 'showTel']" :tel="car.phone">{{ car.phone }}</a>
 
                     <div class="buttons__details">
-                        <button class="btn btn__success">Show phone</button>
+                        <button class="btn btn__success" @click="togglePhone">{{ phoneBtnText }}</button>
                         <button class="btn btn__primary">Buy</button>
                     </div>
                 </div>
@@ -49,26 +56,56 @@
 
         </div>
 
+        <div class="modal">
+            <h3>Do you want by this car?</h3>
+            <p>Modal body text going here</p>
+            <span>X</span>
+            <div class="modal__btn">
+                <button class="btn btn__save">Save changes</button>
+                <button class="btn btn__close">Close</button>
+            </div>
+        </div>
+
+        <div class="backdrop"></div>
+
     </div>
 </template>
 
 <script>
     export default {
-        carsArr: [
-            { name: "Ford", model: "Focus", owner: "Andrey", year: 2016, phone: "+7-929-123-45-67", image: "./dist/ford_focus.jpg" },
-            { name: "Ford", model: "Mondeo", owner: "Evgeniy", year: 2014, phone: "+7-907-321-54-67", image: "./assets/ford_mondeo.jpg" },
-            { name: "Porche", model: "Panamera", owner: "Anatoliy", year: 2018, phone: "+7-600-123-45-67", image: "./assets/porche_panamera.jpg" },
-        ],
         name: "Cars",
         data() { // базовые методы для отрисовки
            return {
-               car: carsArr[0],
+               cars: [
+                   { name: "Ford", model: "Focus", owner: "Andrey", year: 2016, phone: "+7-929-123-45-67", image: "./src/assets/ford_focus.jpg" },
+                   { name: "Volkswagen", model: "Passat", owner: "Evgeniy", year: 2014, phone: "+7-907-321-54-67", image: "./src/assets/passat.jpg" },
+                   { name: "Porche", model: "Panamera", owner: "Anatoliy", year: 2018, phone: "+7-600-123-45-67", image: "./src/assets/porsche_panamera.jpg" },
+                  ],
+               car: { name: "Ford", model: "Focus", owner: "Andrey", year: 2016, phone: "+7-929-123-45-67", image: "./src/assets/ford_focus.jpg" },
+               selectedCarIndex: 0,
+               isActiveTel: false,
+               search: '',
                }
            },
 
         methods: { // указываем функции которые хотим определить
                 selectCar: function(index) {
-                 console.log(this.cars)
+                    this.car = this.cars[index];
+                    this.selectedCarIndex = index;
+            },
+            togglePhone: function() {
+                    this.isActiveTel = !this.isActiveTel
+            }
+        },
+
+        computed: {  //  вычисленные свойства
+            phoneBtnText: function() {
+               return this.isActiveTel ? 'Hide phone' : 'Show phone'
+            },
+            filteredCars: function() {
+                return this.cars.filter(car => {
+                    return car.name.toLocaleLowerCase().indexOf(this.search) > -1 || car.model.toLocaleLowerCase().includes(this.search);
+                })
             }
         }
     }
@@ -124,7 +161,7 @@
             left: -6px;
             background-color: #cccccc;
             display: block;
-            width: 102%;
+            width: 104%;
             height: 1px;
         }
 
@@ -144,11 +181,13 @@
 
     .details__title {
         text-align: center;
+        width: 200px;
+        height: 210px;
     }
 
     .details__image {
         width: 200px;
-        height: auto;
+        height: 133px;
     }
 
     // cars__details
@@ -164,7 +203,7 @@
 
     .details__phone {
         display: block;
-        font-size: 15px;
+        font-size: 18px;
         font-weight: 200;
         margin-bottom: 10px;
     }
@@ -182,6 +221,7 @@
     }
 
     .btn__success {
+        width: 121px;
         border: 1px solid limegreen;
         color: limegreen;
         margin-right: 3px;
@@ -199,6 +239,79 @@
 
         &:hover {
             background: lighten(dodgerblue, 10%);
+        }
+    }
+    // add class to
+    .activeCarElem {
+        background: dodgerblue;
+    }
+    // add class to show or hide phone
+    .showTel {
+        opacity: 0;
+    }
+
+    //  modal
+    .backdrop {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(170,170,170, .5);
+    }
+
+    .modal {
+        background-color: #ffffff;
+        position: relative;
+        top: -250px;
+        margin: auto;
+        width: 350px;
+        height: auto;
+        z-index: 10;
+        border: 1px solid #000;
+        border-radius: 6px;
+        padding: 0 20px;
+
+        h3 {
+            font-weight: 100;
+
+            &:after {
+                content: '';
+                display: block;
+                position: absolute;
+                left: 0;
+                top: 55px;
+                width: 100%;
+                height: 1px;
+                background-color: #000000;
+            }
+        }
+
+        p {
+            padding-top: 10px;
+
+            &:after {
+                content: '';
+                display: block;
+                position: absolute;
+                left: 0;
+                top: 110px;
+                width: 100%;
+                height: 1px;
+                background-color: #000000;
+            }
+        }
+
+        span {
+            position: absolute;
+            top: 20px;
+            right: 20px;
+            font-size: 20px;
+            cursor: pointer;
+
+            &:hover {
+                opacity: .5;
+            }
         }
     }
 
---
+++ src/Modal.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="modal" class="modal">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Modal"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
